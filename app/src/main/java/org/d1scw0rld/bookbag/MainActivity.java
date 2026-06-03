3e018e2eaa20502b7a3b5ede4d194196c63896b1
Iasen Hristozov <Iasen.Hristozov@ingenico.com>
2026-06-03 18:26:37 +0300

    feature/f2025092401-refactoring_to_a_single_activity_application
    MainActivity has been cleaned up. Unused imports, commented-out Navigation UI configuration, and obsolete fragment transaction logic have been removed.

--- app/src/main/java/org/d1scw0rld/bookbag/MainActivity.java
+++ app/src/main/java/org/d1scw0rld/bookbag/MainActivity.java
@@ -4,10 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
 
 /**
@@ -20,37 +16,11 @@ import androidx.navigation.ui.NavigationUI;
  */
 public class MainActivity extends AppCompatActivity
 {
-//   public final static int SHOW_EDIT_BOOK_COPY = 102;
-//
-//   private NavController navController;
-
    @Override
    protected void onCreate(Bundle savedInstanceState)
    {
       super.onCreate(savedInstanceState);
-//      setContentView(R.layout.activity_frame_content);
-//      setContentView(R.layout.activity_navhost);
       setContentView(R.layout.activity_main);
       getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE );
-
-//      navController = Navigation.findNavController(this, R.id.fragment);
-//      NavigationUI.setupActionBarWithNavController(this, navController);
-
-
-      if(savedInstanceState == null)
-      {
-//         BookListFragment fragment = new BookListFragment();
-//
-//         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-////         fragmentTransaction.add(R.id.frame_content, fragment).commit();
-//         fragmentTransaction.add(R.id.fragment, fragment).commit();
-
-      }
    }
-
-//   @Override
-//   public boolean onSupportNavigateUp()
-//   {
-//      return NavigationUI.navigateUp(navController, (DrawerLayout) null);
-//   }
 }
